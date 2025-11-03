42cb6d52d98e972d05f48a7ec984912103322e77
Philipp-Timm <philipptimm25@gmail.com>
2025-11-03 04:52:18 +0100

    chore(SystestPlugin): fixed plugin for CLion2025.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,55 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.kotlin.jvm") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.7.1"
 }
 
 group = "com.example"
-version = "1.3-SNAPSHOT"
+version = "1.4-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2024.1.4")
-    type.set("CL") // Target IDE Platform
+// Configure IntelliJ Platform Gradle Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        clion("2025.2.2")
 
-    plugins.set(listOf("com.intellij.clion", "com.intellij.cidr.lang", "com.intellij.cidr.base", "com.intellij.nativeDebug"/* Plugin Dependencies */))
+        // Add necessary plugin dependencies for compilation here, example:
+        bundledPlugin("com.intellij.clion")
+        bundledPlugin("com.intellij.modules.cidr.lang")
+        bundledPlugin("com.intellij.cidr.lang")
+        bundledPlugin("com.intellij.clion.cmake")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("232")
-        untilBuild.set("254");
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
     }
 }
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
